a0125a41297950561fcd7bae9b7d5f753e408cb4
Kim Blomqvist <kim.blomqvist@lekane.com>
2020-01-22 20:07:50 +0200

    Updates

---
+++ precache-manifest.04ab3b4bb194eb9d2a42f7f3c47eea88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88feada02982e0b12f4284a1e0079394",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc083196e180a4ae207e",
+    "url": "./static/css/main.2ba1d99b.chunk.css"
+  },
+  {
+    "revision": "e0e2b84e7b5395edad6c",
+    "url": "./static/js/2.7693f4ca.chunk.js"
+  },
+  {
+    "revision": "2fbf99eb7b46913e26be09ed5a76f580",
+    "url": "./static/js/2.7693f4ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc083196e180a4ae207e",
+    "url": "./static/js/main.e0c5b527.chunk.js"
+  },
+  {
+    "revision": "4872ffe09b5530a8a3a8",
+    "url": "./static/js/runtime-main.d9733c9f.js"
+  }
+]);
